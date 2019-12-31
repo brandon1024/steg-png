8e07a15e2dd48cdd0d83f23ad10cc149bc7156ca
Brandon Richardson <brandon1024.br@gmail.com>
2019-12-31 07:43:56 -0400

    Finished Extract Builtin Implementation

--- src/builtin/extract.c
+++ src/builtin/extract.c
@@ -14,7 +14,7 @@
 
 #define BUFF_LEN 1024
 #define DEFLATE_CHUNK_DATA_LENGTH 8192
-#define DEFLATE_STREAM_BUFFER_SIZE (128 * 1024)
+#define DEFLATE_STREAM_BUFFER_SIZE 16384
 
 static int extract(const char *, const char *, int);
 static int inflate_data(unsigned char *, size_t, int);
@@ -34,8 +34,8 @@ int cmd_extract(int argc, char *argv[])
 	};
 
 	const struct command_option extract_cmd_options[] = {
-			OPT_STRING('o', "--output", "file", "alternate output file directory", &output_file),
-			OPT_LONG_BOOL("hexdump", "print a hexdump of the embedded data instead of writing to file", &hexdump),
+			OPT_STRING('o', "--output", "file", "alternate output file path", &output_file),
+			OPT_LONG_BOOL("hexdump", "print a hexdump of the embedded data", &hexdump),
 			OPT_BOOL('h', "help", "show help and exit", &help),
 			OPT_END()
 	};
@@ -63,11 +63,6 @@ static int extract(const char *input_file, const char *output_file, int show_hex
 	else
 		strbuf_attach_fmt(&output_file_path, "%s.out", input_file);
 
-	// stat and open descriptor to input file
-	struct stat st;
-	if (lstat(input_file, &st) && errno == ENOENT)
-		FATAL("failed to stat %s'", input_file);
-
 	int in_fd = open(input_file, O_RDONLY);
 	if (in_fd < 0)
 		FATAL(FILE_OPEN_FAILED, input_file);
@@ -88,13 +83,13 @@ static int extract(const char *input_file, const char *output_file, int show_hex
 		DIE("input file is not a PNG (does not conform to RFC 2083)");
 
 	// allocate buffers for inflate input/output
-	struct strbuf in;
-	strbuf_init(&in);
-	strbuf_grow(&in, DEFLATE_STREAM_BUFFER_SIZE);
+	unsigned char *input_buffer = (unsigned char *) malloc(sizeof(unsigned char) * DEFLATE_STREAM_BUFFER_SIZE);
+	if (!input_buffer)
+		FATAL(MEM_ALLOC_FAILED);
 
-	struct strbuf out;
-	strbuf_init(&out);
-	strbuf_grow(&out, DEFLATE_STREAM_BUFFER_SIZE);
+	unsigned char *output_buffer = (unsigned char *) malloc(sizeof(unsigned char) * DEFLATE_STREAM_BUFFER_SIZE);
+	if (!output_buffer)
+		FATAL(MEM_ALLOC_FAILED);
 
 	// set up zlib for deflate
 	struct z_stream_s strm;
@@ -104,12 +99,11 @@ static int extract(const char *input_file, const char *output_file, int show_hex
 	strm.avail_in = 0;
 	strm.next_in = Z_NULL;
 
-	int ret;
-	ret = inflateInit(&strm);
+	int ret = inflateInit(&strm);
 	if (ret != Z_OK)
 		FATAL("failed to initialize zlib for DEFLATE: %s", zError(ret));
 
-	char chunk_type[] = { 's', 't', 'E', 'G' };
+	const char steg_chunk_type[] = {'s', 't', 'E', 'G' };
 	int has_next_chunk, IEND_found = 0;
 	while ((has_next_chunk = chunk_iterator_has_next(&ctx)) != 0) {
 		if (has_next_chunk < 0)
@@ -121,15 +115,22 @@ static int extract(const char *input_file, const char *output_file, int show_hex
 		if (IEND_found)
 			DIE("non-compliant input file with IEND chunk defined twice (does not conform to RFC 2083)");
 
-		if (!memcmp(ctx.current_chunk.chunk_type, chunk_type, CHUNK_TYPE_LENGTH)) {
+		/*
+		 * If this chunk is our stEG chunk, inflate the data and write to the
+		 * temporary file.
+		 * */
+		if (!memcmp(ctx.current_chunk.chunk_type, steg_chunk_type, CHUNK_TYPE_LENGTH)) {
 			ssize_t bytes_read = 0;
-			while ((bytes_read = chunk_iterator_read_data(&ctx, (unsigned char *)in.buff, DEFLATE_STREAM_BUFFER_SIZE)) > 0) {
-				strm.avail_in = bytes_read;
-				strm.next_in = (unsigned char *)in.buff;
 
+			while ((bytes_read = chunk_iterator_read_data(&ctx, input_buffer, DEFLATE_STREAM_BUFFER_SIZE)) > 0) {
+				strm.avail_in = bytes_read;
+				strm.next_in = input_buffer;
+
+				// fully consume the input buffer data
 				do {
 					strm.avail_out = DEFLATE_STREAM_BUFFER_SIZE;
-					strm.next_out = (unsigned char *)out.buff;
+					strm.next_out = output_buffer;
+
 					ret = inflate(&strm, Z_NO_FLUSH);
 					switch (ret) {
 						case Z_STREAM_ERROR:
@@ -141,11 +142,9 @@ static int extract(const char *input_file, const char *output_file, int show_hex
 							break;
 					}
 
-					size_t have = DEFLATE_STREAM_BUFFER_SIZE - strm.avail_out;
-					if (recoverable_write(tmp_fd, strm.next_out, have) != have)
-						FATAL("failed to write inflated data to output file");
-
-					strbuf_remove(&out, 0, have);
+					size_t data_to_write = DEFLATE_STREAM_BUFFER_SIZE - strm.avail_out;
+					if (recoverable_write(tmp_fd, output_buffer, data_to_write) != data_to_write)
+						FATAL("failed to write inflated data to temporary file.");
 				} while (strm.avail_out == 0);
 			}
 		}
@@ -155,12 +154,19 @@ static int extract(const char *input_file, const char *output_file, int show_hex
 	}
 
 	(void)inflateEnd(&strm);
-	strbuf_release(&in);
-	strbuf_release(&out);
+	free(input_buffer);
+	free(output_buffer);
 	chunk_iterator_destroy_ctx(&ctx);
 
 	if (!IEND_found)
 		DIE("non-compliant input file with no IEND chunk defined (does not conform to RFC 2083)");
+
+	// check that the input file actually contained embedded stEG chunks
+	struct stat tmp_file_st;
+	if (fstat(tmp_fd, &tmp_file_st) && errno == ENOENT)
+		FATAL("failed to stat tmp file with descriptor %s'", tmp_fd);
+	if (!tmp_file_st.st_size)
+		DIE("input file is clean; embedded data could not be found.");
 
 	if (show_hexdump) {
 		off_t offset = lseek(tmp_fd, 0, SEEK_SET);
@@ -176,21 +182,23 @@ static int extract(const char *input_file, const char *output_file, int show_hex
 		if (offset < 0)
 			return -1;
 
+		struct stat input_file_st;
+		if (lstat(input_file, &input_file_st) && errno == ENOENT)
+			FATAL("failed to stat %s'", input_file);
+
 		int out_fd = open(output_file_path.buff,  O_WRONLY | O_CREAT | O_TRUNC,
-				st.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO));
+				input_file_st.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO));
 		if (out_fd < 0)
 			FATAL(FILE_OPEN_FAILED, output_file);
 
-		if (fstat(tmp_fd, &st) && errno == ENOENT)
-			FATAL("failed to stat %s'", input_file);
-
-		if (copy_file_fd(out_fd, tmp_fd) != st.st_size)
+		if (copy_file_fd(out_fd, tmp_fd) != tmp_file_st.st_size)
 			FATAL("Failed to write to file %s", output_file_path.buff);
 	}
 
 	close(in_fd);
 	close(tmp_fd);
 	strbuf_release(&output_file_path);
+
 	return 0;
 }
 
